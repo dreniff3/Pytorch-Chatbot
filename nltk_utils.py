b47ca53a2b066487e8fb1d7c2ddfa4dd670cd678
Don Reniff <donaldreniff@gmail.com>
2024-09-07 17:48:19 -0700

    Add functions for tokenizing and stemming

---
+++ nltk_utils.py
@@ -0,0 +1,38 @@
+'''
+    This module...
+'''
+
+import nltk
+from nltk.stem.porter import PorterStemmer
+
+# download pre-trained tokenizer so .word_tokenize works
+# nltk.data.path = [r'C:\Users\Don Reniff\AppData\Roaming\nltk_data']
+# nltk.download('punkt')
+
+# create stemmer
+stemmer = PorterStemmer()
+
+
+def tokenize(sentence):
+    '''
+        Splits a sentence into meaningful units.
+    '''
+    return nltk.word_tokenize(sentence)
+
+
+def stem(word):
+    '''
+        Generates the root form of words (using a crude heuristic that chops
+        the end of words).
+    '''
+    return stemmer.stem(word.lower())
+
+
+def bag_of_words(tokenized_sentence, all_words):
+    pass
+
+
+s = "How long does shipping take?"
+print(s)
+s = tokenize(s)
+print(s)
